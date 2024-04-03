5ebff9787aec7c24137f5cc919672afc25902fe8
Jason A. Novak <1934060+jasonanovak@users.noreply.github.com>
2024-04-02 22:14:03 -0700

    Revert "delete"
    This reverts commit bc4779153bbdbf8f0fb9031dd046547da2d83d5a.

--- src/src/background.js
+++ src/src/background.js
@@ -26,197 +26,198 @@ const debugFlag = false;
  * 
  * @param command The command sent by the user to resize and move the window.
  */
-async function updateWindowPos(command){
-  if (debugFlag){
-  console.log("command is ", command);
-  }
-
-  var currentWindow = await chrome.windows.getCurrent();
-  var currentWindowId = currentWindow.id;
-  console.log("currentWindowId is ", currentWindowId);
-
-  var tempWindow = await chrome.windows.create({
-                                                url:"http://nymag.com",
-                                                state: "maximized"});
-
-  const maxWindowHeight = tempWindow.height;
-  console.log("maxWindowHeight is ", maxWindowHeight);
-  const maxWindowWidth = tempWindow.width;
-  console.log("maxWindowWidth is ", maxWindowWidth);
-  const maxWindowTop = tempWindow.top;
-  console.log("maxWindowTop is ", maxWindowTop);
-  const maxWindowLeft = tempWindow.left;
-  console.log("maxWindowLeft is ", maxWindowLeft);
-  const tempWindowId = tempWindow.id;
-
-  if (true){
-    console.log("tempWindow is ", tempWindow);
-  }
-
-//  await chrome.windows.remove(tempWindowId);
-
-  /**
-   * command is one of the values in the commands array in manifest.json.
-   * It specifies the size to resize the window to and what position the
-   * window should move to.
-   */
-  
-  if (command == "11-quarters-top-right" ||
-      command == "13-quarters-bottom-right" ||
-      command == "12-quarters-bottom-left" ||
-      command == "10-quarters-top-left"
-      ){
-    
-    /**
-     * updateHeight and updateWidth are the new values to resize the
-     * window to; they're determined by the command.
-     */
-    
-    updateHeight = parseInt(maxWindowHeight/2);
-    updateWidth = parseInt(maxWindowWidth/2);
-
-    if (command == "11-quarters-top-right" ||
-        command == "10-quarters-top-left"){
-      
+function updateWindowPos(command){
+  console.log(command);
+  currentWindow = chrome.windows.getCurrent();
+  currentWindow.then(currentWindowResolve => {
+    if (debugFlag){
+      console.log("currentWindowResolve is ", currentWindowResolve);
+    }
+    displayInfo = chrome.system.display.getInfo();
+    displayInfo.then(displayInfoResolve => {
+      if (debugFlag){
+        console.log("displayInfoResolve is ", displayInfoResolve);
+      }
       /**
-       * updateTop is the distance from the top of the screen to move the
-       * window to.
+       * height and width are the height and width of the display the current
+       * window is on.
        * 
-       * updateLeft is the distance from the left hand side of the screen
-       * to move the window to.
+       * They're used to calculate the new window size.
        * 
-       * The top left corner is 0,0.
-       * 
-       * These are calculated on whether the window should be  1/6, 1/4, 
-       * 1/3, or 1/2 of the screen's size.
+       * workArea is used because it is "The usable work area of the display 
+       * within the display bounds. The work area excludes areas of the 
+       * display reserved for OS, for example taskbar and launcher." per 
+       * https://developer.chrome.com/docs/extensions/reference/api/system/display#:~:text=physical%20tablet%20state.-,workArea,-Bounds
        */
       
-      updateTop = maxWindowTop;
-    }
-    else if (command == "13-quarters-bottom-right" ||
-              command == "12-quarters-bottom-left"){
-      updateTop = maxWindowHeight/2;
-    }
+      let height = displayInfoResolve[0].workArea.height;
+      let width = displayInfoResolve[0].workArea.width;
 
-    if (command == "12-quarters-bottom-left" ||
-        command == "10-quarters-top-left"){
-      updateLeft = maxWindowLeft;
-    }
-    else if (command == "11-quarters-top-right" ||
-              command == "13-quarters-bottom-right"){
-      updateLeft = maxWindowWidth/2;
-    }
-  }
-
-  if (command == "14-halves-top" ||
-      command == "15-halves-bottom" ||
-      command == "16-halves-left" ||
-      command == "17-halves-right"){
-    if (command == "14-halves-top" ||
-        command == "15-halves-bottom"){
-      updateHeight = parseInt(maxWindowHeight/2);
-      updateWidth = maxWindowWidth;
-      updateLeft = maxWindowLeft;
-      if (command == "14-halves-top"){
-        updateTop = maxWindowTop;
-      }
-      else if (command == "15-halves-bottom"){
-        updateTop = parseInt(maxWindowHeight/2);
-      }
-    }
-    else if (command == "16-halves-left" ||
-              command == "17-halves-right"){
-      updateHeight = maxWindowHeight;
-      updateWidth = parseInt(maxWindowWidth/2);
-      updateTop = maxWindowTop;
-      if (command == "16-halves-left"){
-        updateLeft = maxWindowLeft;
-      }
-      else if (command == "17-halves-right"){
-        updateLeft = parseInt(maxWindowWidth/2);
-      }
-    }
-  }
-
-  if (command == "01-third-left" ||
-      command == "02-third-center" ||
-      command == "03-third-right"){
-    updateTop = maxWindowTop;
-    updateHeight = maxWindowHeight;
-    updateWidth = parseInt(maxWindowWidth/3);
-    if (command == "01-third-left"){
-      updateLeft = maxWindowLeft;
-    }
-    else if (command == "02-third-center"){
-      updateLeft = parseInt(maxWindowWidth/3);
-    }
-    else if (command == "03-third-right"){
-      updateLeft = parseInt((maxWindowWidth/3)*2);
-    }
-  }
-
-  if (command == "04-sixth-top-left" ||
-      command == "05-sixth-top-center" ||
-      command == "06-sixth-top-right" ||
-      command == "07-sixth-bottom-left" ||
-      command == "08-sixth-bottom-center" ||
-      command == "09-sixth-bottom-right"){
-    updateHeight = parseInt(maxWindowHeight/2);
-    updateWidth = parseInt(maxWindowWidth/3);
-    if (command == "04-sixth-top-left" ||
-        command == "05-sixth-top-center" ||
-        command == "06-sixth-top-right"){
-      updateTop = maxWindowTop;
-    }
-    else if (command == "07-sixth-bottom-left" ||
-              command == "08-sixth-bottom-center" ||
-              command == "09-sixth-bottom-right"){
-      updateTop = parseInt(maxWindowHeight/2);
-    }
-
-    if (command == "04-sixth-top-left" ||
-        command == "07-sixth-bottom-left"){
-        updateLeft = maxWindowLeft;
-    }
-    else if (command == "05-sixth-top-center" ||
-              command == "08-sixth-bottom-center"){
-      updateLeft = parseInt(maxWindowWidth/3);
-    }
-    else if (command == "06-sixth-top-right" ||
-              command == "09-sixth-bottom-right"){
-      updateLeft = parseInt((maxWindowWidth/3)*2);
-    }
-  }
-  
-  /**
-   * updateInfo is the dictionary that specifies the new size and location
-   * of the window.
-   */
-
-  let updateInfo = {"height": updateHeight,
-                    "width": updateWidth,
-                    "top": updateTop,
-                    "left": updateLeft,
-                    "state": "normal",
-                    "drawAttention": false};
-  if (debugFlag){
-    console.log("updating to ", command, " ", updateInfo);
-  }
-  /**
-   * Create the promise to resize and move the window and then execute.
-   */
-
-//  windowUpdate = await chrome.windows.update(currentWindowId, 
-//                                             updateInfo);
+      /**
+       * command is one of the values in the commands array in manifest.json.
+       * It specifies the size to resize the window to and what position the
+       * window should move to.
+       */
       
+      if (command == "11-quarters-top-right" ||
+          command == "13-quarters-bottom-right" ||
+          command == "12-quarters-bottom-left" ||
+          command == "10-quarters-top-left"
+          ){
+        
+        /**
+         * updateHeight and updateWidth are the new values to resize the
+         * window to; they're determined by the command.
+         */
+        
+        updateHeight = parseInt(height/2);
+        updateWidth = parseInt(width/2);
 
-};
+        if (command == "11-quarters-top-right" ||
+            command == "10-quarters-top-left"){
+          
+          /**
+           * updateTop is the distance from the top of the screen to move the
+           * window to.
+           * 
+           * updateLeft is the distance from the left hand side of the screen
+           * to move the window to.
+           * 
+           * The top left corner is 0,0.
+           * 
+           * These are calculated on whether the window should be  1/6, 1/4, 
+           * 1/3, or 1/2 of the screen's size.
+           */
+          
+          updateTop = displayInfoResolve[0].workArea.top;
+        }
+        else if (command == "13-quarters-bottom-right" ||
+                 command == "12-quarters-bottom-left"){
+          updateTop = displayInfoResolve[0].workArea.height/2;
+        }
+
+        if (command == "12-quarters-bottom-left" ||
+            command == "10-quarters-top-left"){
+          updateLeft = displayInfoResolve[0].workArea.left;
+        }
+        else if (command == "11-quarters-top-right" ||
+                 command == "13-quarters-bottom-right"){
+          updateLeft = displayInfoResolve[0].workArea.width/2;
+        }
+      }
+
+      if (command == "14-halves-top" ||
+          command == "15-halves-bottom" ||
+          command == "16-halves-left" ||
+          command == "17-halves-right"){
+        if (command == "14-halves-top" ||
+            command == "15-halves-bottom"){
+          updateHeight = parseInt(height/2);
+          updateWidth = width;
+          updateLeft = displayInfoResolve[0].workArea.left;
+          if (command == "14-halves-top"){
+            updateTop = displayInfoResolve[0].workArea.top;
+          }
+          else if (command == "15-halves-bottom"){
+            updateTop = parseInt(height/2);
+          }
+        }
+        else if (command == "16-halves-left" ||
+                 command == "17-halves-right"){
+          updateHeight = height;
+          updateWidth = parseInt(width/2);
+          updateTop = displayInfoResolve[0].workArea.top;
+          if (command == "16-halves-left"){
+            updateLeft = displayInfoResolve[0].workArea.left;
+          }
+          else if (command == "17-halves-right"){
+            updateLeft = parseInt(width/2);
+          }
+        }
+      }
+
+      if (command == "01-third-left" ||
+          command == "02-third-center" ||
+          command == "03-third-right"){
+        updateTop = displayInfoResolve[0].workArea.top;
+        updateHeight = height;
+        updateWidth = parseInt(width/3);
+        if (command == "01-third-left"){
+          updateLeft = displayInfoResolve[0].workArea.left;
+        }
+        else if (command == "02-third-center"){
+          updateLeft = parseInt(width/3);
+        }
+        else if (command == "03-third-right"){
+          updateLeft = parseInt((width/3)*2);
+        }
+      }
+
+      if (command == "04-sixth-top-left" ||
+          command == "05-sixth-top-center" ||
+          command == "06-sixth-top-right" ||
+          command == "07-sixth-bottom-left" ||
+          command == "08-sixth-bottom-center" ||
+          command == "09-sixth-bottom-right"){
+        updateHeight = parseInt(height/2);
+        updateWidth = parseInt(width/3);
+        if (command == "04-sixth-top-left" ||
+            command == "05-sixth-top-center" ||
+            command == "06-sixth-top-right"){
+          updateTop = displayInfoResolve[0].workArea.top;
+        }
+        else if (command == "07-sixth-bottom-left" ||
+                 command == "08-sixth-bottom-center" ||
+                 command == "09-sixth-bottom-right"){
+          updateTop = parseInt(height/2);
+        }
+
+        if (command == "04-sixth-top-left" ||
+            command == "07-sixth-bottom-left"){
+           updateLeft = displayInfoResolve[0].workArea.left;
+        }
+        else if (command == "05-sixth-top-center" ||
+                 command == "08-sixth-bottom-center"){
+          updateLeft = parseInt(width/3);
+        }
+        else if (command == "06-sixth-top-right" ||
+                 command == "09-sixth-bottom-right"){
+          updateLeft = parseInt((width/3)*2);
+        }
+      }
+      
+      /**
+       * updateInfo is the dictionary that specifies the new size and location
+       * of the window.
+       */
+
+      let updateInfo = {"height": updateHeight,
+                        "width": updateWidth,
+                        "top": updateTop,
+                        "left": updateLeft,
+                        "state": "normal",
+                        "drawAttention": false};
+      if (debugFlag){
+        console.log("updating to ", command, " ", updateInfo);
+      }
+      /**
+       * Create the promise to resize and move the window and then execute.
+       */
+      
+      windowUpdatePromise = chrome.windows.update(chrome.windows.WINDOW_ID_CURRENT,
+                                                  updateInfo);
+      windowUpdatePromise.then();
+
+    });
+  });
+}
+
 
 /**
  * This listens for the command sent by the user via keyboard shortcut.
  */
 chrome.commands.onCommand.addListener((command) => {
-  updateWindowPos(command); 
+  updateWindowPos(command);
 });
 
 chrome.runtime.onInstalled.addListener(({ reason }) => {
